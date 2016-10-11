a3d329c04f855aacf703de26d300f244421939c5
Kevin Farmer <farmekp0@sewanee.edu>
2016-10-11 08:56:34 -0400

    First commit.

---
+++ basics.py
@@ -0,0 +1,49 @@
+class Person(object):
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.friends = []
+
+    def greet(self, other_person):
+        print 'Hello %s, I am %s!' % (other_person.name, self.name)
+
+    def print_contact_info(self):
+        print "%s's email: %s; %s's phone number: %s " % (self.name, self.email, self.name, self.phone)
+
+    def add_friend(self):
+        self.friends.append()
+
+    def num_friends(self):
+        print len(self.friends)
+
+sonny = Person('Sonny', 'sonny@hotmail.com', '483-485-4948')
+
+jordan = Person('Jordan', 'jordan@aol.com', '495-586-3456')
+
+#sonny.greet(jordan)
+#jordan.greet(sonny)
+
+#print sonny.email, sonny.phone
+#print jordan.email, jordan.phone
+
+#class Vehicle(object):
+#    def __init__(self, make, model, year):
+#        self.make = make
+#        self.model = model
+#        self.year = year
+#
+#    def print_info(self):
+#        print self.year, self.make, self.model
+
+#mustang = Vehicle('Ford', 'Mustang', 2009)
+#mustang.print_info()
+
+#sonny.print_contact_info()
+#jordan.print_contact_info()
+
+sonny.add_friend(jordan)
+jordan.add_friend(sonny)
+
+#sonny.num_friends()
+#jordan.num_friends()
